e62dcc55b444da0e040f03d0590580df7eb5c2bd
kwanhyo <lhc2472@gmail.com>
2023-10-12 00:21:30 +0900

    11.22_2 commit chapter5

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,16 +1,73 @@
 import "./Body.css"
-
+import { useState } from "react";
+/* p225
 const Body = ({children}) => {
-    const handleOnClick = (e) =>{
-        console.log(e)
-        console.log(e.target.name)
+    console.log('update');
+    const [count, setCount] = useState(0);
+    const onIncrease = () => {
+        setCount(count + 1);
     }
     return(
-        <div className="body">
-            <button name="A버튼"onClick={handleOnClick}>A버튼</button>
-            <button name="B버튼"onClick={handleOnClick}>B버튼</button>
+        <div>
+            <h2>{count}</h2>
+            <button onClick={onIncrease}>+</button>
         </div>
     )
 }
 
+const Body = () => {
+    const [text, setText] = useState("");
+    const handleOnChange = (e) => {
+        console.log("변경된 값 "+ e.target.value);
+        setText(e.target.value);
+    };
+    return (
+        <div>
+            <textarea value={text} onChange={handleOnChange} />
+            <div>{text}</div>
+        </div>
+    )
+}*/
+
+const Body = () => {
+    const [name, setName] = useState("");
+    const [gender, setGender] = useState("");
+    const [birth, setBirth] = useState("");
+    const [bio, setBio] = useState("");
+
+    const onChangeName = (e) => {
+        setName(e.target.value);
+    };
+    const onChangeGender = (e) => {
+        setGender(e.target.value);
+    };
+    const onChangeBirth = (e) => {
+        setBirth(e.target.value);
+    };
+    const onChangeBio = (e) => {
+        console.log(e)
+        setBio(e.target.value);
+    };
+
+    return (
+        <div>
+            <div>
+                <input value={name} onChange = {onChangeName} placeholder = "이름"/>
+            </div>
+            <div>
+                <select value={gender} onChange = {onChangeGender}>
+                    <option key={""}></option>
+                    <option key={"남성"}>남성</option>
+                    <option key={"여성"}>여성</option>
+                </select>
+            </div>
+            <div>
+                <input type="date" value={birth} onChange ={onChangeBirth} />
+            </div>
+            <div>
+                <textarea value={bio} onChange = {onChangeBio} /> 
+            </div>
+        </div>
+    );
+}
 export default Body;
